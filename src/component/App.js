7191634968fcc7f06d02fcfc1589c58976b2d621
Kirk Visser <kirkviss@gmail.com>
2019-12-09 23:37:55 -0500

    Make side bar a layer instead of a box

--- src/component/App.js
+++ src/component/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
 import CategoryLayer from './CategoryLayer.js'
-import { Grid, Grommet, Box, Text, Button} from 'grommet';
+import { Grid, Grommet, Box, Text, Button, Layer } from 'grommet';
 import { grommet } from "grommet/themes";
 import { introduction, resume } from '../content'
-
+import { FormClose, Menu } from "grommet-icons";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      sidebar: true
+      navbar: false
     }
   }
   render() {
@@ -20,14 +20,15 @@ export default class App extends React.Component {
         <Grommet full theme={grommet}>
           <Grid
             fill
-            columns={['auto','flex']}
-            rows={['auto','flex']}
+            columns={['auto', 'flex']}
+            rows={['auto', 'flex']}
             areas={[
-              {name : "header", start: [0,0], end: [1,0] },
-              {name : "sidebar", start: [0,1], end: [0,1] },
-              { name: "main", start: [1, 1], end: [1, 1] }
+              { name: "header", start: [0, 0], end: [1, 0] },
+              { name: "main", start: [0, 1], end: [1, 1] },
+              // {name : "sidebar", start: [1,1], end: [1,1] }
+
             ]}
-          
+
           >
             {/* -- Header -- */}
             <Box
@@ -38,48 +39,87 @@ export default class App extends React.Component {
               pad={{ horizontal: "medium", vertical: "medium" }}
               background="brand"
             >
-              <Button onClick={() => this.setState({sidebar: !this.state.sidebar})}>
-                <Text size="large">Kirk Visser</Text>
-              </Button>
               <Text>kirkviss@gmail.com</Text>
+              <Button 
+                onClick={() => this.setState({ navbar: !this.state.navbar })} 
+                icon={<Menu /> }
+              />
+
+
             </Box>
             {/* -- Sidebar -- */}
-            {this.state.sidebar && (
-              <Box
-                gridArea="sidebar"
-                background="brand"
-                width="small"
-                animation={[
-                  { type: "fadeIn", duration: 300 },
-                  { type: "slideRight", size:"large", duration: 150 }
-                ]}
+
+
+            {this.state.navbar && (
+              <Layer
+                full="vertical"
+                position="right"
+                onEsc={this.state.navbar}
               >
-                {["First", "Second", "Third"].map(name => (
-                  <Button key={name} href="#" hoverIndicator>
-                    <Box pad={{ horizontal: "medium", vertical: "small" }}>
-                      <Text>{name}</Text>
-                    </Box>
-                  </Button>
-                ))}
-              </Box>
+                <Box
+                  direction="row"
+                  align="center"
+                  as="header"
+                  elevation="small"
+                  background="brand"
+                  justify="between"
+                >
+
+                  <Button 
+                    onClick={() => this.setState({ navbar: !this.state.navbar })}
+                    icon={<FormClose />} />
+                </Box>
+                <Box
+                  fill
+                  overflow="auto"
+                  background="brand"
+                  width="small"
+                  animation={[
+                    { type: "fadeIn", duration: 300 },
+                    { type: "slideLeft", size: "large", duration: 150 }
+                  ]}
+                >
+                  {["First", "Second", "Third"].map(label => (
+                    <Button
+                      hoverIndicator
+                      plain
+                    >
+                      <Box
+                        key={label}
+                        href="#"
+
+                        pad={{ horizontal: "medium", vertical: "medium" }}
+                      >
+                        <Text size="large"> {label}</Text>
+
+                      </Box >
+                    </Button>
+                  ))}
+
+                </Box>
+              </Layer>
             )}
+
+
+
+
             {/* -- Main --  */}
-            <Box 
+            <Box
               gridArea="main"
               justify="stretch"
               align="center"
               fill
               flex
               direction='column'
-              
+
             >
               < CategoryLayer content={introduction} />
-              < CategoryLayer content={resume}  />  
+              < CategoryLayer content={resume} />
             </Box>
-        
+
           </Grid>
 
-          
+
         </Grommet>
 
       </div>
---
+++ src/component/intro_annoncement.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import {Box, AnnounceContext} from 'grommet'
+
+export default () => (
+    <>
+);
