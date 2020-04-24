67d5132934cd25814b50ca2c608bd93ba4cff5dd
Kirk Visser <kirkviss@gmail.com>
2020-04-24 11:55:42 -0400

    Remove commented out experience component

--- src/component/main/experience.js
+++ src/component/main/experience.js
@@ -72,65 +72,6 @@ const Experience = (props) => (
             ))}
         </Accordion>
         </Box>
-    //     <Box
-    //         id="Experience"
-    //         {...props}
-    //     >
-    //         <Position data={ExperienceData[1]} />
-    //         <Markdown>{
-    //             `# Experience 
-
-    // ### Software Engineer
-
-    // **Capital One**, New York, NY
-
-    // 5/2020 - Present
-
-    // ### Infrastructure Software Engineer II
-
-    // **TD Ameritrade**, Jersey City, NJ
-
-    // 1/2020 - 4/2020
-
-    // [Node.js](#node.js), [JaveScript](#skills), [Nginx](#skill)
-
-    // ### Infrastructure Software Engineer I
-
-    // **TD Ameritrade**, Jersey City, NJ	
-
-    // 6/2018 - 1/2020
-
-    // [Node.js](#skills), [JaveScript](#skills), [Nginx](#skill), [Python](#Python)
-
-    // ### IAAS and Cloud Engineering Co-op
-
-    // **TD Ameritrade**, Jersey City, NJ
-
-    // 5/2016 - 8/2016 & 1/2017 - 8/2017
-
-    // [Node.js](#skills), [JaveScript](#skills), [Nginx](#skill)
-
-    // ### QA Co-op
-
-    // **Crestron Electronics**, Rockleigh, NJ
-
-    // 8/2015 - 12/2015
-
-    // [Node.js](#skills), [JaveScript](#skills), [Nginx](#skill)
-
-    // ### Software Development Co-op
-
-    // **MIAC Analytics,** New York, NY
-
-    // 1/2015 – 5/2015
-
-    // [Node.js](#/Node.JS), [JaveScript](#skills), [Nginx](#skill)
-
-
-
-    // `
-    //         }</Markdown>
-    //     </Box>
 )
 
 export default Experience;
