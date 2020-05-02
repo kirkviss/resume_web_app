1188a68bf27051a0ef0fcccdb84104ec432b5b7f
Kirk Visser <kirkviss@gmail.com>
2020-05-02 10:58:16 -0400

    Add todo items in comments of index file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './static/index.css';
 import App from './component/App';
 import * as serviceWorker from './serviceWorker';
+// TODO Create a dark mode
+// TODO Create a container class/variable that contains the structure of the eduction and experience containers to remove duplication
+// TODO Remove the navbar and sidebar logic from the App container, for abstraction and to minimize code on the app page
+// TODO Add a logging system
+// TODO Implement a testing system
+// TODO Remove hard code values where possible
 
 
 
