2c2025c6b1fdfb34edf32ee109d550ac0fc962de
Kirk Visser <kirkviss@gmail.com>
2020-05-02 22:52:05 -0400

    Add to todo list and add nginx to the docker image

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,16 @@
-# Dockerfile 
-FROM node:current-slim
+# Dockerfile
+#Stage 1: build the application
+FROM node:current-slim as build-dep
+RUN mkdir /app
 WORKDIR /app
 COPY package.json ./
 COPY package-lock.json ./
 RUN npm ci
 COPY . ./
 RUN npm run build
-EXPOSE 3000
-CMD ["npx", "serve", "-s", "build", "-l", "3000"]
+
+# Stage 2: Create the production environemnt
+FROM nginx:1.13.12-alpine
+COPY --from=build-dep /app/build /usr/share/nginx/html
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './static/index.css';
 import App from './component/App';
 import * as serviceWorker from './serviceWorker';
+// TODO Rewrite the readme to reflect how the application runs
 // TODO Create a dark mode
 // TODO Create a container class/variable that contains the structure of the eduction and experience containers to remove duplication
 // TODO Remove the navbar and sidebar logic from the App container, for abstraction and to minimize code on the app page
 // TODO Add a logging system
 // TODO Implement a testing system
 // TODO Remove hard code values where possible
+
 
 
 
